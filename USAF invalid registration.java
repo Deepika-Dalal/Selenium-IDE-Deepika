ca296ea19d5587e4d3f45dbc9417f696576a3e37
Deepika Dalal <deepika.dalal@weboniselab.com>
2016-09-14 23:03:00 +0530

    USAF invalid registration

---
+++ USAF invalid registration.java
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="selenium.base" href="https://www2-beta.usafootball.com/register/coaching" />
+<title>USAF invalid registration</title>
+</head>
+<body>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
+<tr><td rowspan="1" colspan="3">USAF invalid registration</td></tr>
+</thead><tbody>
+<tr>
+	<td>open</td>
+	<td>/register/coaching</td>
+	<td></td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewContactFirstname</td>
+	<td>test</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewContactLastname</td>
+	<td>coach</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewContactLine1</td>
+	<td>bavdhan</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewContactCity</td>
+	<td>pune</td>
+</tr>
+<tr>
+	<td>select</td>
+	<td>id=NewContactStateId</td>
+	<td>label=California</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewContactZip</td>
+	<td>12345</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewContactPhoneNumber</td>
+	<td>1234567890</td>
+</tr>
+<tr>
+	<td>select</td>
+	<td>id=NewContactBirthdayMonth</td>
+	<td>label=October</td>
+</tr>
+<tr>
+	<td>select</td>
+	<td>id=NewContactBirthdayDay</td>
+	<td>label=17</td>
+</tr>
+<tr>
+	<td>select</td>
+	<td>id=NewContactBirthdayYear</td>
+	<td>label=1998</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewUserUsername</td>
+	<td>coach_live32@weboapps.com</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewUserConfirmUsername</td>
+	<td>coach_live32@weboapps.com</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewUserPassword</td>
+	<td>11111111</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=NewUserConfirmPassword</td>
+	<td>11111111</td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>id=autocomplete</td>
+	<td>test </td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>id=37456</td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>id=submit</td>
+	<td></td>
+</tr>
+
+</tbody></table>
+</body>
+</html>
